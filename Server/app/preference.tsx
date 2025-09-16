ec058c72dbf2630d10413721ce8bb38073c8fef3
Reinierwalle <reinier-vd-walle@hotmail.com>
2025-09-16 11:52:10 +0200

    tsx fix

--- Server/app/preference.tsx
+++ Server/app/preference.tsx
@@ -6,36 +6,39 @@ import SpawnPanel from "./spwan";
 import InfoPanel from "./info";
 import ColorsPanel from "./colors";
 import { useState } from "react";
+import { Key } from "react"; // Import Key type
 
 export default function Preference() {
-
     const [color, setColor] = useState("default");
 
-    return <div className="flex flex-col max-w-xl ">
-        <Card className="p-8">
-            <Tabs
-                aria-label="Options"
-                color={color as any}
-                onSelectionChange={(key: string) => {
-                    if (key === "colors") setColor("default"); // default
-                    if (key === "wifi") setColor("success"); // green
-                    if (key === "spawn") setColor("primary"); // blue
-                    if (key === "info") setColor("danger"); // red
-                }}
-            >
-                <Tab key="colors" title="Colors">
-                    <ColorsPanel />
-                </Tab>
-                <Tab key="wifi" title="WiFi">
-                    <WiFiPanel />
-                </Tab>
-                <Tab key="spawn" title="Spawn">
-                    <SpawnPanel />
-                </Tab>
-                <Tab key="info" title="Info">
-                    <InfoPanel />
-                </Tab>
-            </Tabs>
-        </Card>
-    </div>;
+    return (
+        <div className="flex flex-col max-w-xl ">
+            <Card className="p-8">
+                <Tabs
+                    aria-label="Options"
+                    color={color as any}
+                    onSelectionChange={(key: Key) => { 
+                        const k = String(key); 
+                        if (k === "colors") setColor("default"); // default
+                        if (k === "wifi") setColor("success"); // green
+                        if (k === "spawn") setColor("primary"); // blue
+                        if (k === "info") setColor("danger"); // red
+                    }}
+                >
+                    <Tab key="colors" title="Colors">
+                        <ColorsPanel />
+                    </Tab>
+                    <Tab key="wifi" title="WiFi">
+                        <WiFiPanel />
+                    </Tab>
+                    <Tab key="spawn" title="Spawn">
+                        <SpawnPanel />
+                    </Tab>
+                    <Tab key="info" title="Info">
+                        <InfoPanel />
+                    </Tab>
+                </Tabs>
+            </Card>
+        </div>
+    );
 }
